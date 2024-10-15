f4855c2f597a30936b019f34d29af52109ecef3b
Saravanan M <sarvan.m20@gmail.com>
2024-10-15 17:50:33 +0530

    setup done

---
+++ question1.js
@@ -0,0 +1,16 @@
+// Question 1: Normalize an Input String (Easy)
+// Problem Statement
+// Write a program that normalizes a given input string. Normalization includes the
+// following steps:
+// 1. Remove any extra spaces from the beginning and end of the string.
+// 2. Remove any special characters and punctuation, leaving only
+// alphanumeric characters and spaces.
+// 3. Replace multiple spaces between words with a single space.
+// 4. Convert the cleaned string to camel case (i.e., the first letter of each word
+// is capitalized, and all other letters are in lowercase).
+// Input
+// A single string that may contain uppercase letters, extra spaces, and special
+// characters.
+// Output
+// A single normalized string in camel case.
+
